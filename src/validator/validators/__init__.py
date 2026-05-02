9be2f07f08e2c18b2755beedfccfda2df29c202e
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-05-02 18:52:19 +0500

    feat(validators): добавлен CircularDependencyValidator в публичный API

--- src/validator/validators/__init__.py
+++ src/validator/validators/__init__.py
@@ -4,10 +4,12 @@ from validator.validators.base_validator import BaseValidator
 from validator.validators.anchor_link import AnchorLinkValidator
 from validator.validators.broken_link import BrokenLinkValidator
 from validator.validators.orphan_file import OrphanFileValidator
+from validator.validators.circular_deps import CircularDependencyValidator
 
 __all__ = [
     'BaseValidator',
     'AnchorLinkValidator',
     'BrokenLinkValidator',
     'OrphanFileValidator',
+    'CircularDependencyValidator',
 ]
