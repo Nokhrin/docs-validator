3be7d103c86c69b0e9fc56b7589fa054dcf72e35
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-05-10 19:00:15 +0500

    feat: реализованы классы генераторов отчетов json, html

--- src/validator/reporters/__init__.py
+++ src/validator/reporters/__init__.py
@@ -3,9 +3,15 @@
 """
 
 from validator.reporters.base_reporter import BaseReporter
+from validator.reporters.cli_reporter import CLIReporter
 from validator.reporters.html_reporter import HTMLReporter
+from validator.reporters.json_reporter import JSONReporter
 from validator.reporters.markdown_reporter import MarkdownReporter
 
 __all__ = [
-    'BaseReporter', 'MarkdownReporter', 'HTMLReporter',
+    'BaseReporter',
+    'MarkdownReporter',
+    'HTMLReporter',
+    'JSONReporter',
+    'CLIReporter',
 ]
